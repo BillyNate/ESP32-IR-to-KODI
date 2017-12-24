e421dcd1b713e8e2ad3181aa21c1cc5e2db78980
Nate <billynate@billynate.com>
2017-12-25 00:21:03 +0100

    Added a button with a listener

--- code.js
+++ code.js
@@ -1,10 +1,12 @@
 var $;
-(function() {
+(function()
+{
     // Load the script
-    var script = document.createElement("SCRIPT");
+    var script = document.createElement("script");
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js';
     script.type = 'text/javascript';
-    script.onload = function() {
+    script.onload = function()
+    {
         $ = window.jQuery;
         init();
     };
@@ -13,5 +15,24 @@ var $;
 
 function init()
 {
-    $('body').css({ 'background-color': '#00f' });
+    $('<button>').text('Listen...').appendTo('body').on('click', function()
+    {
+        var button = this,
+            sec = 1,
+            interval = setInterval(function()
+            {
+                $(button).text('Listen' + Array(sec+1).join('.'));
+                sec ++
+                if(sec > 3)
+                {
+                    sec = 1;
+                }
+            }, 500);
+        $.get('/listen', function(data)
+        {
+            clearInterval(interval);
+            $(button).text('Listen...');
+            $('<p>').text(data).appendTo('body');
+        });
+    });
 }
